5432734cf73d286af99d71439ed88c0fab4c64f6
rifkigg <rifkiadi348@gmail.com>
2025-02-28 22:01:37 +0700

    (change) mengganti icon

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'name' => 'LaravelPWA',
+    'name' => 'InvenGo',
     'manifest' => [
         'name' => env('APP_NAME', 'InvenGo'),
         'short_name' => 'InvenGo',
@@ -13,49 +13,49 @@ return [
         'status_bar'=> 'black',
         'icons' => [
             '72x72' => [
-                'path' => '/images/icons/icon-72x72.png',
+                'path' => '/images/invenGo-logo.jpg',
                 'purpose' => 'any'
             ],
             '96x96' => [
-                'path' => '/images/icons/icon-96x96.png',
+                'path' => '/images/invenGo-logo.jpg',
                 'purpose' => 'any'
             ],
             '128x128' => [
-                'path' => '/images/icons/icon-128x128.png',
+                'path' => '/images/invenGo-logo.jpg',
                 'purpose' => 'any'
             ],
             '144x144' => [
-                'path' => '/images/icons/icon-144x144.png',
+                'path' => '/images/invenGo-logo.jpg',
                 'purpose' => 'any'
             ],
             '152x152' => [
-                'path' => '/images/icons/icon-152x152.png',
+                'path' => '/images/invenGo-logo.jpg',
                 'purpose' => 'any'
             ],
             '192x192' => [
-                'path' => '/images/icons/icon-192x192.png',
+                'path' => '/images/invenGo-logo.jpg',
                 'purpose' => 'any'
             ],
             '384x384' => [
-                'path' => '/images/icons/icon-384x384.png',
+                'path' => '/images/invenGo-logo.jpg',
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => '/images/icons/icon-512x512.png',
+                'path' => '/images/invenGo-logo.jpg',
                 'purpose' => 'any'
             ],
         ],
         'splash' => [
-            '640x1136' => '/images/icons/splash-640x1136.png',
-            '750x1334' => '/images/icons/splash-750x1334.png',
-            '828x1792' => '/images/icons/splash-828x1792.png',
-            '1125x2436' => '/images/icons/splash-1125x2436.png',
-            '1242x2208' => '/images/icons/splash-1242x2208.png',
-            '1242x2688' => '/images/icons/splash-1242x2688.png',
-            '1536x2048' => '/images/icons/splash-1536x2048.png',
-            '1668x2224' => '/images/icons/splash-1668x2224.png',
-            '1668x2388' => '/images/icons/splash-1668x2388.png',
-            '2048x2732' => '/images/icons/splash-2048x2732.png',
+            '640x1136' => '/images/invenGo-logo.jpg',
+            '750x1334' => '/images/invenGo-logo.jpg',
+            '828x1792' => '/images/invenGo-logo.jpg',
+            '1125x2436' => '/images/invenGo-logo.jpg',
+            '1242x2208' => '/images/invenGo-logo.jpg',
+            '1242x2688' => '/images/invenGo-logo.jpg',
+            '1536x2048' => '/images/invenGo-logo.jpg',
+            '1668x2224' => '/images/invenGo-logo.jpg',
+            '1668x2388' => '/images/invenGo-logo.jpg',
+            '2048x2732' => '/images/invenGo-logo.jpg',
         ],
         'shortcuts' => [
             [
